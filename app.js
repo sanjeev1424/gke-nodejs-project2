4123b0fe7d1eba32606506d6f9e9c292e1d1dec2
sanjeev1424 <sanjeev.yadav1424@gmail.com>
2025-10-27 20:43:35 +0530

    new files created in clouddeploy folder for automation using cloud deploy

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get('/health', (req, res) => res.json({status: 'ok'}));
 app.get('*', (req, res) => {
 res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+// for rollback rollout and versioning testing---for new public/deploy-status.html page testing.......  
+app.get('/version.json', (req, res) => {
+  res.json({
+    image: process.env.IMAGE_TAG || 'unknown',
+    updatedAt: new Date().toISOString()
+  });
+});
+
 
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
